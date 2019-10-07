b52c823ee71266e5851a2241537629b9f32fd16f
Lucas <rorattol@gmail.com>
2019-10-06 21:09:16 -0300

    Criando conexão com o banco de dados Postgres

---
+++ src/br/csi/dao/ConectDB_postgres.java
@@ -0,0 +1,32 @@
+package br.csi.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectDB_postgres {
+    private static final String url = "jdbc:postgresql://localhost:5432/";
+    private static final String user = "postgres";
+    private static final String password = "12345";
+
+
+    /**
+     * TESTE
+     * @return CONEXAO DB
+     */
+    public Connection getConexao(){
+        Connection conn = null;
+
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+}
