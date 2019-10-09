9ca0837967714f76eda80f4c0fa9717ab0be6621
Lucas <rorattol@gmail.com>
2019-10-09 17:11:33 -0300

    Criação das classe no br.csi.model

--- src/br/csi/model/Material.java
+++ src/br/csi/model/Material.java
@@ -1,4 +1,56 @@
 package br.csi.model;
 
+//CREATE TABLE material(
+//        id_material int not null,
+//        nome_material varchar (50),
+//        unidade_medida varchar (50),
+//        id_categoria int,
+//        primary key (id_material),
+//        foreign key (id_categoria) references categoria(id_categoria)
+//        );
 public class Material {
+
+    private int idMaterial;
+    private String nomeMaterial;
+    private String unidadeMedida;
+    private Categoria categoria;
+
+    public Material(int idMaterial, String nomeMaterial, String unidadeMedida, Categoria categoria) {
+        this.idMaterial = idMaterial;
+        this.nomeMaterial = nomeMaterial;
+        this.unidadeMedida = unidadeMedida;
+        this.categoria = categoria;
+    }
+
+    public int getIdMaterial() {
+        return idMaterial;
+    }
+
+    public void setIdMaterial(int idMaterial) {
+        this.idMaterial = idMaterial;
+    }
+
+    public String getNomeMaterial() {
+        return nomeMaterial;
+    }
+
+    public void setNomeMaterial(String nomeMaterial) {
+        this.nomeMaterial = nomeMaterial;
+    }
+
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    public void setUnidadeMedida(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
--- src/br/csi/model/Usuario.java
+++ src/br/csi/model/Usuario.java
@@ -1,4 +1,92 @@
 package br.csi.model;
 
+//CREATE TABLE usuario(
+//        id_usuario int not null,
+//        login varchar(50),
+//        senha varchar,
+//        situacao boolean,
+//        primary key (id_usuario),
+//        id_tipousuario int,
+//        cod_setor int,
+//        foreign key (cod_setor) references setor (cod_setor),
+//        foreign key (id_tipousuario) references tipo_usuario (id_tipousuario)
+//        );
 public class Usuario {
+
+    private int idUsuario;
+    private String nomeUsuario;
+    private String loginUsuario;
+    private String senhaUsuario;
+    private boolean situacao;  //true=ativo false=inativo
+    private String tipoUsuario;
+    private Setor setor;
+
+    public Usuario(int idUsuario, String nomeUsuario, String loginUsuario, String senhaUsuario, boolean situacao, String tipoUsuario, Setor setor) {
+        this.idUsuario = idUsuario;
+        this.nomeUsuario = nomeUsuario;
+        this.loginUsuario = loginUsuario;
+        this.senhaUsuario = senhaUsuario;
+        this.situacao = situacao;
+        this.tipoUsuario = tipoUsuario;
+        this.setor = setor;
+    }
+
+    public Usuario() {
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getLoginUsuario() {
+        return loginUsuario;
+    }
+
+    public void setLoginUsuario(String loginUsuario) {
+        this.loginUsuario = loginUsuario;
+    }
+
+    public String getSenhaUsuario() {
+        return senhaUsuario;
+    }
+
+    public void setSenhaUsuario(String senhaUsuario) {
+        this.senhaUsuario = senhaUsuario;
+    }
+
+    public boolean isSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(boolean situacao) {
+        this.situacao = situacao;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Setor getSetor() {
+        return setor;
+    }
+
+    public void setSetor(Setor setor) {
+        this.setor = setor;
+    }
 }
